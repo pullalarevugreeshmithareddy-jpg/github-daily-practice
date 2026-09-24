66f537b28eb07c9b90432851dd12a1d5c122b06a
Greeshmitha <pullalarevugreeshmithareddy@gmail.com>
2026-09-24 12:39:03 +0530

    Day 2: Learned Python variables and data types

---
+++ day02.py
@@ -0,0 +1,16 @@
+# Python Variables and Data Types
+
+name = "Greeshmitha"
+age = 22
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print("Name data type:", type(name))
+print("Age data type:", type(age))
+print("Height data type:", type(height))
+print("Student data type:", type(is_student))
