9f8f96ebe97b08a095c411765bab6666dd1d4f47
Greeshmitha <pullalarevugreeshmithareddy@gmail.com>
2026-09-25 08:16:01 +0530

    Day 3: Learned Python user input

---
+++ day03.py
@@ -0,0 +1,22 @@
+# Day 3 - Python User Input
+
+# Getting text from the user
+name = input("Enter your name: ")
+
+# Getting numbers from the user
+age = int(input("Enter your age: "))
+
+# Getting a decimal number
+height = float(input("Enter your height in feet: "))
+
+# Displaying the information
+print()
+print("----- My Details -----")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Simple calculation
+age_after_5_years = age + 5
+
+print("Your age after 5 years:", age_after_5_years)
