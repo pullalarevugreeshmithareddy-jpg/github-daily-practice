64acc35a4a2eb7c8fc642c3a6aee41f21bc4ae74
Greeshmitha <pullalarevugreeshmithareddy@gmail.com>
2026-09-26 08:40:34 +0530

    Day 4: Learned Python operators

---
+++ day04.py
@@ -0,0 +1,16 @@
+# Day 4 - Python Operators
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print()
+print("----- Results -----")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
